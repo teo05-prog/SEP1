61ff7c2c210ce679b229d2f56d54a0257e9b6c5d
Elizabeth1401 <140805762+Elizabeth1401@users.noreply.github.com>
2024-12-03 13:24:00 +0100

    Pet part

---
+++ src/Cat.java
@@ -0,0 +1,44 @@
+public class Cat extends Pet
+{
+  private String breed;
+  private String breederName;
+
+  public Cat (String name, int age, String colour, char gender, String comment, String breed, String breederName){
+    super(name, age, colour, gender, comment);
+    this.breed=breed;
+    this.breederName=breederName;
+  }
+
+  public String getBreed(){
+    return breed;
+  }
+  public String getBreederName(){
+    return breederName;
+  }
+  public void setBreed(String breed){
+    this.breed=breed;
+  }
+  public void setBreederName(String breederName){
+    this.breederName=breederName;
+  }
+
+  public Pet copy(){
+    return new Cat(super.getName(),getAge(),getColour(),getGender(),getComment(),breed,breederName);
+  }
+  public String toString(){
+    return "Cat: "+super.toString()+", breed: "+breed+", breeder name: "+breederName+"/n";
+  }
+  public boolean equals(Object obj){
+    if (obj==null || getClass()!=obj.getClass()){
+      return false;
+    }
+    Cat other=(Cat) obj;
+    return other.getName().equals(super.getName())&&
+        other.getAge()==super.getAge()&&
+        other.getColour().equals(super.getColour())&&
+        other.getGender()==super.getGender()&&
+        other.getComment().equals(super.getComment())&&
+        other.getBreed().equals(breed)&&
+        other.getBreederName().equals(breederName);
+  }
+}
---
+++ src/Dog.java
@@ -0,0 +1,44 @@
+public class Dog extends Pet
+{
+  private String breed;
+  private String breederName;
+
+  public Dog(String name, int age, String colour, char gender, String comment, String breed, String breederName){
+    super(name, age, colour, gender, comment);
+    this.breed=breed;
+    this.breederName=breederName;
+  }
+
+  public String getBreed(){
+    return breed;
+  }
+  public String getBreederName(){
+    return breederName;
+  }
+  public void setBreed(String breed){
+    this.breed=breed;
+  }
+  public void setBreederName(String breederName){
+    this.breederName=breederName;
+  }
+
+  public Pet copy(){
+    return new Dog(super.getName(),getAge(),getColour(),getGender(),getComment(),breed,breederName);
+  }
+  public String toString(){
+    return "Dog: "+super.toString()+", breed: "+breed+", breeder name: "+breederName+"/n";
+  }
+  public boolean equals(Object obj){
+    if (obj==null || getClass()!=obj.getClass()){
+      return false;
+    }
+    Dog other=(Dog) obj;
+    return other.getName().equals(super.getName())&&
+        other.getAge()==super.getAge()&&
+        other.getColour().equals(super.getColour())&&
+        other.getGender()==super.getGender()&&
+        other.getComment().equals(super.getComment())&&
+        other.getBreed().equals(breed)&&
+        other.getBreederName().equals(breederName);
+  }
+}
