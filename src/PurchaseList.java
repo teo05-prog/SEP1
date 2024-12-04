6deff7468ab1627857df67c88dd68721fd541bc8
bianca0203 <biancabuzdugan00@gmail.com>
2024-12-05 01:19:43 +0200

    added Date class
    Took 6 minutes

---
+++ src/PurchaseList.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+public class PurchaseList
+{
+  private ArrayList<Purchase> purchases;
+  private Purchase purchase;
+
+  public PurchaseList()
+  {
+    purchases = new ArrayList<>();
+  }
+  public void addPurchase(Purchase purchase)
+  {
+    purchases.add(purchase);
+  }
+  public void removePurchase(Purchase purchase)
+  {
+    purchases.remove(purchase);
+  }
+  public Purchase getPurchase(int index) throws Exception
+  {
+    if (index >= 0 && index < purchases.size())
+    {
+      return purchases.get(index);
+    }
+    else
+    {
+      throw new IndexOutOfBoundsException("Invalid index");
+    }
+  }
+  public String toString()
+  {
+    String str = "";
+    for (Purchase purchase : purchases)
+    {
+      if (purchase != null)
+      {
+        str += purchase.toString();
+      }
+    }
+    return str;
+  }
+
+ public boolean equals(Object obj)
+  {
+    if (obj == null & getClass() != obj.getClass())
+    {
+      return false;
+    }
+    PurchaseList other = (PurchaseList) obj;
+    return purchases.equals(other.purchases);
+  }
+}
